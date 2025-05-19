4ef0bf383dbd9e282bc3cbb22e8be9450d815b44
777one777 <jcuevavasquez2025@gmail.com>
2025-05-19 14:28:05 -0700

    Initial Commit

---
+++ SummerOlympics/SummerOlympicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SummerOlympicsApp.swift
+//  SummerOlympics
+//
+//  Created by Student2 on 5/19/25.
+//
+
+import SwiftUI
+
+@main
+struct SummerOlympicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
